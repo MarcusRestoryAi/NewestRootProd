ef5373bc45efd1c4c1599f2bcd66f1278b31a102
Marcus <marcus.henriksson@restory.ai>
2023-09-29 09:47:43 +0200

    Added menu functionallity for user, as well as being able to fetch JSON to Class object

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,7 @@ import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
@@ -19,12 +20,16 @@ import org.springframework.kafka.support.serializer.JsonDeserializer;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws MalformedURLException, org.json.simple.parser.ParseException {
         System.out.println("Hello world!");
+
+        userMenu();
 
         /*
 
@@ -39,13 +44,69 @@ public class Main {
         consumer.subscribe(Collections.singletonList("your-topic"));
         */
 
+
+
+        //Hämta data från topic
+        //getDataFromKafka("javaJsonGuides");
+
+        //System.out.println("Project done!");
+    }
+
+    public static void userMenu() {
+        String userChoise = "";
+
+        do {
+
+            printMenu();
+
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Gör ditt val: ");
+            userChoise = scan.nextLine();
+
+            //SwitchCase
+            switch (userChoise) {
+                case "1": {
+                    userInputForKafka();
+                    break;
+                }
+                case "2": {
+                    getDataFromKafka("javaJsonGuides");
+                    break;
+                }
+                case "0": {
+                    break;
+                }
+                default: {
+                    System.out.println("Felaktig input. Försök igen");
+                    break;
+                }
+            }
+
+            if (!userChoise.equals("0")) {
+                System.out.println("Press any key to continue");
+                scan.nextLine();
+            }
+
+        } while (!userChoise.equals("0"));
+    }
+
+    public static void printMenu() {
+        /* Writing up the meny */
+        System.out.println("Gör dit val!");
+        System.out.println("------------");
+        System.out.println("1. Skriv data till Kafka Server");
+        System.out.println("2. Hämta data från Kafka Server");
+        System.out.println("0. Avsluta");
+    }
+
+    public static void userInputForKafka() {
         User user = new User();
 
         /*Logik flr att låta användaren mata in data*/
 
-        user.setId(10L);
-        user.setFirstName("Niklas");
-        user.setLastName("Cullberg");
+        user.setId(11L);
+        user.setFirstName("Cihan");
+        user.setLastName("Vivera");
 
         JSONObject myObj = new JSONObject();
         myObj.put("id", user.getId());
@@ -55,12 +116,7 @@ public class Main {
         // URL url = new URL("http://localhost:8080/api/v1/kafka/publish");
 
         //Skicka Payload till WebAPI via en Request
-        //sendToWebAPI(myObj);
-
-        //Hämta data från topic
-        getDataFromKafka("javaJsonGuides");
-
-        System.out.println("Project done!");
+        sendToWebAPI(myObj);
     }
 
     public static void sendToWebAPI(JSONObject myObj) {
@@ -85,26 +141,35 @@ public class Main {
         } catch (IOException e) { e.printStackTrace(); }
     }
 
-    public static void getDataFromKafka(String topicName) throws org.json.simple.parser.ParseException {
+    public static void getDataFromKafka(String topicName) {
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "fetchingGroup");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        //props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
         //props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.springframework.kafka.support.serializer.JsonDeserializer");
+        //props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.springframework.kafka.support.serializer.JsonDeserializer");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put("spring.json.trusted.packages", "*");
 
         Consumer<String, User> consumer = new KafkaConsumer<>(props);
         //consumer.subscribe(Collections.singletonList(topicName));
         consumer.assign(Collections.singletonList(new TopicPartition(topicName, 0)));
 
+        //Gå till början av Topic
         consumer.seekToBeginning(consumer.assignment());
+
+        //Create User list
+        ArrayList<User> users = new ArrayList<User>();
 
         //WhileLoop osm hämtar i JSON format
         while (true) {
             ConsumerRecords<String, User> records = consumer.poll(Duration.ofMillis(100));
+            if (records.isEmpty()) continue;
             for (ConsumerRecord<String, User> record : records) {
-                System.out.println(record);
+                users.add(record.value());
+                //System.out.println(record.value());
+                //System.out.println(record.value().getClass().toString());
 /*
                 //Spara datan tillbaka till ett JSONObject
                 JSONObject fetchData = (JSONObject) new JSONParser().parse(record.value());
@@ -116,6 +181,11 @@ public class Main {
 
 */
             }
+            break;
+        }
+
+        for (User user : users) {
+            System.out.println(user.getFirstName());
         }
 /*
         while (true) {
